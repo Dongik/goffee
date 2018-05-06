3bcf44e494098b95d06574194a7b662b00dae67b
Ubuntu <ubuntu@ip-172-26-10-224.ap-northeast-1.compute.internal>
2018-05-06 12:44:26 +0000

    RemoteAddr

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 // dongik's main function
 func indexHandler(w http.ResponseWriter, r *http.Request) {
     fmt.Printf("URL.Path = %q\n", r.URL.Path)
-    fmt.Printf("URL = %q\n", r.Host)
+    fmt.Printf("URL = %q\n", r.RemoteAddr)
     t := template.New("main")
     t, _ = template.ParseFiles("order.html")  // Parse template file.
     t.Execute(w,"Hello world")  // merge.
@@ -25,5 +25,5 @@ func main(){
 	r := mux.NewRouter()
     r.HandleFunc("/",indexHandler);
 	r.HandleFunc("/orders", orderHandler).Methods("POST")
-	http.ListenAndServe(":3000", r); 
+	http.ListenAndServe(":8080", r); 
 }
