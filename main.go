0ec679bad02512a5c6650e82a514f9a59590e399
dongik <dongik3475@gmail.com>
2018-05-06 12:20:36 +0900

    web site works

--- main.go
+++ main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
+    "html/template"
 	"net/http"
 	"github.com/gorilla/mux"
+    "fmt"
 )
 
+
 // dongik's main function
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Printf("URL.Path = %q\n", r.URL.Path)
+    fmt.Printf("URL = %q\n", r.Host)
+    t := template.New("main")
+    t, _ = template.ParseFiles("order.html")  // Parse template file.
+    t.Execute(w,"Hello world")  // merge.
+}
+func orderHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+
 func main(){
-
-
+	r := mux.NewRouter()
+    r.HandleFunc("/",indexHandler);
+	r.HandleFunc("/orders", orderHandler).Methods("POST")
+	http.ListenAndServe(":3000", r); 
 }
